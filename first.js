f1837ed6098f819b4b45a4802d750afd209ad973
JayJawalkar <jawalkarjay7@gmail.com>
2025-03-12 22:38:56 +0530

    first commit

---
+++ first.js
@@ -0,0 +1,18 @@
+const product={
+    productName:"Abc",
+    rating:"*****",
+    dealOfDay:true,
+    price:"$270"
+}
+
+const instagram={
+    userName:"Jay",
+    isVerified:true,
+    ifFollow:false,
+    posts:100,
+    followers:15552020,
+    following:4854,
+    bio:"_____________________________________________"
+}
+
+console.log(instagram.userName)
